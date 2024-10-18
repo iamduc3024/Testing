bddaa04ec35880d696de110c32510c13dd63906d
iamduc3024 <iamduc3024@gmail.com>
2024-10-18 18:10:37 +0700

    change test case data flow testing

--- Testing/src/test/java/org/example/ServiceTestBasedOnDataFlowTesting.java
+++ Testing/src/test/java/org/example/ServiceTestBasedOnDataFlowTesting.java
@@ -22,8 +22,8 @@ class ServiceTestBasedOnDataFlowTesting {
 
     @Test
     void testCase2() {
-        weight = 1000f;
-        height = 1000f;
+        weight = 1f;
+        height = 0f;
 
         assertThrows(IllegalArgumentException.class, () -> {
             service.healthStatusBasedOnBMI(weight, height);
@@ -32,6 +32,26 @@ class ServiceTestBasedOnDataFlowTesting {
 
     @Test
     void testCase3() {
+        weight = 1000f;
+        height = 1;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.healthStatusBasedOnBMI(weight, height);
+        });
+    }
+
+    @Test
+    void testCase4() {
+        weight = 600f;
+        height = 3;
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.healthStatusBasedOnBMI(weight, height);
+        });
+    }
+
+    @Test
+    void testCase5() {
         weight = 20f;
         height = 1.2f;
 
@@ -39,25 +59,25 @@ class ServiceTestBasedOnDataFlowTesting {
     }
 
     @Test
-    void testCase4() {
-        weight = 120.8f;
-        height = 2.2f;
+    void testCase6() {
+        weight = 62f;
+        height = 1.65f;
 
         assertEquals("Normal", service.healthStatusBasedOnBMI(weight, height));
     }
 
     @Test
-    void testCase5() {
-        weight = 170.4f;
-        height = 2.5f;
+    void testCase7() {
+        weight = 67f;
+        height = 1.53f;
 
         assertEquals("Overweight", service.healthStatusBasedOnBMI(weight, height));
     }
 
     @Test
-    void testCase6() {
-        weight = 225f;
-        height = 1.7f;
+    void testCase8() {
+        weight = 72f;
+        height = 1.48f;
 
         assertEquals("Obesity", service.healthStatusBasedOnBMI(weight, height));
     }
